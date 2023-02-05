d751f9c0812494b04ff46091385723642232bc52
Amanishwa <119420408+Amanishwa@users.noreply.github.com>
2023-02-05 16:31:15 +0530

    Netflix

--- App.js
+++ App.js
@@ -1,1 +1,34 @@
+Const TabItems = Document.QuerySelectorAll('.Tab-Item');
+Const TabContentItems = Document.QuerySelectorAll('.Tab-Content-Item');
 
+// Select Tab Content Item
+Function SelectItem(E) {
+  // Remove All Show And Border Classes
+  RemoveBorder();
+  RemoveShow();
+  // Add Border To Current Tab Item
+  This.ClassList.Add('Tab-Border');
+  // Grab Content Item From DOM
+  Const TabContentItem = Document.QuerySelector(`#${This.Id}-Content`);
+  // Add Show Class
+  TabContentItem.ClassList.Add('Show');
+}
+
+// Remove Bottom Borders From All Tab Items
+Function RemoveBorder() {
+  TabItems.ForEach(Item => {
+    Item.ClassList.Remove('Tab-Border');
+  });
+}
+
+// Remove Show Class From All Content Items
+Function RemoveShow() {
+  TabContentItems.ForEach(Item => {
+    Item.ClassList.Remove('Show');
+  });
+}
+
+// Listen For Tab Item Click
+TabItems.ForEach(Item => {
+  Item.AddEventListener('Click', SelectItem);
+});
